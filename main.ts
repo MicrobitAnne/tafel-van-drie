dbb85e9723784aca853b0586b6d687ff5a1c643f
MicrobitAnne <161485257+MicrobitAnne@users.noreply.github.com>
2024-03-11 11:06:26 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 let drie = 0
 basic.forever(function () {
-    if (drie <= 12) {
+    if (drie < 13) {
         basic.showNumber(drie)
         drie += 3
+    } else {
+        drie = 0
+        basic.showNumber(drie)
     }
 })
